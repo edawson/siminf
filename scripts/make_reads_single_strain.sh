ab36067da92b8b9e9ecd44e2f172f03b1421bb35
Eric T. Dawson <eric.t.dawson@gmail.com>
2017-10-12 14:32:46 -0500

    Up reads to 50K in make_reads script

--- scripts/make_reads_single_strain.sh
+++ scripts/make_reads_single_strain.sh
@@ -1,4 +1,4 @@
-~/sandbox/siminf/art_bin_MountRainier/art_illumina -ss MSv3 -sam -i $1 -o `basename $1 .fa` -l 250 -f 10000
+~/sandbox/siminf/art_bin_MountRainier/art_illumina -ss MSv3 -sam -i $1 -o `basename $1 .fa` -l 250 -f 50000
 if [ ! -d alns ]; then mkdir alns; fi
 if [ ! -d fq ]; then mkdir fq; fi
 mv *.fq fq/
